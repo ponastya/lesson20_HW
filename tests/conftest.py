35442dbbd7ef066de8cc322a115bfa297fe54409
ponastya <nastya8po@gmail.com>
2022-12-15 14:04:02 +0300

    movies, genres добавлены в conftest

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,7 +4,13 @@ from unittest.mock import MagicMock
 import pytest
 
 from dao.model.director import Director
+from dao.model.genre import Genre
 from dao.director import DirectorDAO
+from dao.genre import GenreDAO
+
+from dao.model.movie import Movie
+from dao.movie import MovieDAO
+
 from setup_db import db
 
 @pytest.fixture()
@@ -29,3 +35,51 @@ def director_dao():
     director_dao.update = MagicMock()
 
     return director_dao
+
+
+@pytest.fixture()
+def genre_dao():
+    genre_dao = GenreDAO(db.session)
+
+    genre_1 = Genre(id=1, name="Genre 1")
+    genre_2 = Genre(id=2, name="Genre 2")
+    genre_3 = Genre(id=3, name="Genre 3")
+
+    genres = {
+        1: genre_1,
+        2: genre_2,
+        3: genre_3
+    }
+
+    #моки
+    genre_dao.get_one = MagicMock(return_value=genre_1)
+    genre_dao.get_all = MagicMock(return_value=genres.values())
+    genre_dao.create = MagicMock(return_value=genre_1)
+    genre_dao.delete = MagicMock()
+    genre_dao.update = MagicMock()
+
+    return genre_dao
+
+
+@pytest.fixture()
+def movie_dao():
+    movie_dao = MovieDAO(db.session)
+
+    movie_1 = Movie(id=1, title="Movie 1", description="Des 1", trailer="Link 1", year=1000, rating=10.0, genre_id=1, director_id=1)
+    movie_2 = Movie(id=2, title="Movie 2", description="Des 2", trailer="Link 2", year=2000, rating=11.0, genre_id=2, director_id=2)
+    movie_3 = Movie(id=3, title="Movie 3", description="Des 3", trailer="Link 3", year=3000, rating=12.0, genre_id=3, director_id=3)
+
+    movies = {
+        1: movie_1,
+        2: movie_2,
+        3: movie_3
+    }
+
+    #моки
+    movie_dao.get_one = MagicMock(return_value=movie_1)
+    movie_dao.get_all = MagicMock(return_value=movies.values())
+    movie_dao.create = MagicMock(return_value=movie_1)
+    movie_dao.delete = MagicMock()
+    movie_dao.update = MagicMock()
+
+    return movie_dao
